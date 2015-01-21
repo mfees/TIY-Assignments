d46544ffb829f55927e0f95cb8a3bb931a9fb76e
Maddie Fees <madison.fees90@gmail.com>
2015-01-21 13:45:18 -0700

    @jrutishauser @mfees, subtraction yippie

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,15 @@ console.log(add(5, 6));
 
 assert.equal(add(1, 2), 3);
     
+
+function diff(A, B) {
+    
+    return A - B;
+
+}
+
+console.log(diff(10, 3))
+
+assert.equal(diff(6, 3), 3)
+
+assert.equal(diff(7, 3), 4)
