401adfa305cfdc8ccb0f4d8109f569c3cbcc9b66
Maddie Fees <madison.fees90@gmail.com>
2015-01-21 13:59:27 -0700

    @jrutishauser @mfees division assertion

--- scripts/main.js
+++ scripts/main.js
@@ -30,3 +30,14 @@ console.log(prod(6, 7));
 assert.equal(prod(3, 2), 6);
 
 assert.equal(prod(10, 3), 30);
+
+
+function div(A, B) {
+    return A / B;
+    
+}
+
+console.log(div(60, 10));
+
+assert.equal(div(10, 2), 5);
+assert.equal(div(20, 5), 4);
