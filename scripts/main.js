34860f417ce00783db3631c1ab532b8fcf737168
Maddie Fees <madison.fees90@gmail.com>
2015-01-23 14:57:06 -0700

    @jrutishauser @mfees

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,11 @@
 var assert = require('assert');
 
-Array[["zero", 0], ["one", 1], ["two", 2], ["three", 3], ["four", 4], ["five, 5"]];
-
 function plus(A, B) {
-    return A + B;
+    return 0;
 }
 
-plus(1, 2);
-
-console.log(plus(1, 2));
-
-it('make two string numbers into number total', function () {
-    assert.equal(plus("one", "one"), 2);
-    assert.equal(plus("three", "two"), 5);
-    assert.equal(plus("two", "four"), 6);
+it('should add "zero" and "zero"', function() {
+    assert.equal(plus(0, 0), 0);
 });
 
-
-
-
+    
