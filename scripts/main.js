ded29ed41bbd0f6e29e8c4dfa9f2a45d50b32a2a
Maddie Fees <madison.fees90@gmail.com>
2015-01-23 12:32:35 -0700

    adding array values 1-5

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 var assert = require('assert');
 
+Array[["zero", 0], ["one", 1], ["two", 2], ["three", 3], ["four", 4], ["five, 5"]];  
+
 function plus(A, B) {
     return A + B;
 }
@@ -13,5 +15,7 @@ it('make two string numbers into number total', function () {
     assert.equal(plus("three", "two"), 5);
     assert.equal(plus("two", "four"), 6);
 });
+
+
 
 
