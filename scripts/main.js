2ad3a498c6b6820ed2ae34dfcef3d08db591b65b
Maddie Fees <madison.fees90@gmail.com>
2015-01-24 19:00:51 -0700

    string calc zero

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,21 @@
 var assert = require('assert');
 
-function plus(A, B) {
+/*function plus(A, B) {
+    if (A === "zero" && B === "zero");
     return 0;
 }
 
-it('should add "zero" and "zero"', function() {
-    assert.equal(plus(0, 0), 0);
-});
+it('should add "zero" and "zero"', function () {
+    assert.equal(plus("zero", "zero"), 0);
+});*/
 
-    
+
+
+function plus(A, B) {
+    if (A === "one" && B === "zero");
+    return 1;
+}
+
+it ('should add "one" and "zero"', function () {
+    assert.equal(plus("one", "zero"), 1);
+});
