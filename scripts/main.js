4b450e49850c57395c827e19e3d6622247e5ffd8
Maddie Fees <madison.fees90@gmail.com>
2015-01-24 19:37:49 -0700

    check writing six to ten

--- scripts/main.js
+++ scripts/main.js
@@ -110,11 +110,61 @@ it ('should change 4 to english "four"', function () {
 });*/
 
 
-function toEnglish(A) {
+/*function toEnglish(A) {
     if (A === 5);
     return "five";
 }
 
 it ('should change 5 to english "five"', function () {
     assert.equal(toEnglish(5), "five");
+});*/
+
+
+/*function toEnglish(A) {
+    if (A === 6);
+    return "six";
+}
+
+it ('should change 6 to english "six"', function () {
+    assert.equal(toEnglish(6), "six");
+});*/
+
+
+/*function toEnglish (A) {
+    if (A === 7);
+    return "seven";
+}
+
+it ('should change 7 to english "seven"', function () {
+    assert.equal(toEnglish(7), "seven");
+});*/
+
+
+/*function toEnglish(A) {
+    if (A === 8);
+    return "eight";
+}
+
+it ('should change 8 to english "eight"', function () {
+    assert.equal(toEnglish(8), "eight");
+});*/
+
+
+/*function toEnglish(A) {
+    if (A === 9);
+    return "nine";
+}
+
+it ('should change 9 to english "nine"', function () {
+    assert.equal(toEnglish(9), "nine");
+});*/
+
+
+function toEnglish(A) {
+    if (A === 10);
+    return "ten";
+}
+
+it ('should change 10 to english "ten"', function () {
+    assert.equal(toEnglish(10), "ten");
 });
