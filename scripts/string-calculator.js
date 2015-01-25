69a871b12339cd05f06a402782d97547c56ed39f
Maddie Fees <madison.fees90@gmail.com>
2015-01-25 15:12:27 -0700

    oops, did these in the wrong spot, here they are again

---
+++ scripts/string-calculator.js
@@ -0,0 +1,110 @@
+var assert = require('assert');
+
+/*function plus(A, B) {
+    if (A === "zero" && B === "zero");
+    return 0;
+}
+
+it('should add "zero" and "zero"', function () {
+    assert.equal(plus("zero", "zero"), 0);
+});*/
+
+
+/*function plus(A, B) {
+    if (A === "one" && B === "zero");
+    return 1;
+}
+
+it ('should add "one" and "zero"', function () {
+    assert.equal(plus("one", "zero"), 1);
+});*/
+
+
+/*function plus(A, B) {
+    if (A === "two" && B === "zero"); 
+    return 2;
+}
+
+it ('should add "two" and "zero"', function () {
+    assert.equal(plus("two", "zero"), 2);
+});*/
+
+
+/*function plus(A, B) {
+    if (A === "three" && B === "zero");
+    return 3;
+}
+
+it ('should add "three" and "zero"', function () {
+    assert.equal(plus("three", "zero"), 3);
+});*/
+
+
+/*function plus (A, B) {
+    if (A === "four" && B === "zero");
+    return 4;
+}
+
+it ('should add "four" and "zero"', function () {
+    assert.equal(plus("four", "zero"), 4);
+});*/
+
+
+/*function plus(A, B) {
+    if (A === "five" && B === "zero");
+    return 5;
+}
+
+it ('should add "five" and "zero"', function () {
+    assert.equal(plus("five", "zero"), 5);
+});*/
+
+
+/*function plus(A, B) {
+    if (A === "six" && B === "zero");
+    return 6;
+}
+
+it ('should add "six" and "zero"', function () {
+    assert.equal(plus("six", "zero"), 6);
+});*/
+
+
+/*function plus(A, B) {
+    if (A === "seven" &&  B === "zero");
+    return 7;
+}
+
+it ('should add "seven" and "zero"', function () {
+    assert.equal(plus("seven", "zero"), 7);
+});*/
+
+
+/*function plus (A, B) {
+    if (A === "eight" && B === "zero");
+    return 8;
+}
+
+it ('should add "eight" and "zero"', function () {
+    assert.equal(plus("eight", "zero"), 8);
+});*/
+
+
+/*function plus (A, B) {
+    if (A === "nine" && B === "zero");
+    return 9;
+}
+
+it ('should add "nine" and "zero"', function () {
+   assert.equal(plus("nine", "zero"), 9);
+});*/
+
+
+function plus (A, B) {
+    if (A === "ten" && B === "zero");
+    return 10;
+}
+
+if ('should add "ten" and "zero"', function () {
+    assert.equal(plus("ten", "zero"), 10);
+});
