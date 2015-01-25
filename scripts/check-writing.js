ed48d5cd701a9a7fba7f5f1933f08f3360623e28
Maddie Fees <madison.fees90@gmail.com>
2015-01-25 15:13:51 -0700

    did these int he wrong spot too, adding to right file so I can start the chess board

---
+++ scripts/check-writing.js
@@ -0,0 +1,111 @@
+var assert = require('assert');
+
+
+/*function toEnglish (A) {
+    if (A === 0);
+    return "zero"
+}
+
+it ('should change 0 to english "zero"', function () {
+    assert.equal(toEnglish(0), "zero");
+});*/
+
+
+/*function toEnglish (A) {
+    if (A === 1);
+    return "one";
+}
+
+it ('should change 1 to english "one"', function () {
+    assert.equal(toEnglish(1), "one");
+});*/
+
+
+/*function toEnglish(A) {
+    if (A === 2);
+    return "two";
+}
+
+it ('should change 2 to english "two"', function () {
+    assert.equal(toEnglish(2), "two");
+});*/
+
+
+/*function toEnglish(A) {
+    if (A === 3);
+    return "three";
+}
+
+it ('should change 3 to english "three"', function () {
+    assert.equal(toEnglish(3), "three");
+});*/
+
+
+/*function toEnglish(A) {
+    if (A === 4);
+    return "four";
+}
+
+it ('should change 4 to english "four"', function () {
+    assert.equal(toEnglish(4), "four");
+});*/
+
+
+/*function toEnglish(A) {
+    if (A === 5);
+    return "five";
+}
+
+it ('should change 5 to english "five"', function () {
+    assert.equal(toEnglish(5), "five");
+});*/
+
+
+/*function toEnglish(A) {
+    if (A === 6);
+    return "six";
+}
+
+it ('should change 6 to english "six"', function () {
+    assert.equal(toEnglish(6), "six");
+});*/
+
+
+/*function toEnglish (A) {
+    if (A === 7);
+    return "seven";
+}
+
+it ('should change 7 to english "seven"', function () {
+    assert.equal(toEnglish(7), "seven");
+});*/
+
+
+/*function toEnglish(A) {
+    if (A === 8);
+    return "eight";
+}
+
+it ('should change 8 to english "eight"', function () {
+    assert.equal(toEnglish(8), "eight");
+});*/
+
+
+/*function toEnglish(A) {
+    if (A === 9);
+    return "nine";
+}
+
+it ('should change 9 to english "nine"', function () {
+    assert.equal(toEnglish(9), "nine");
+});*/
+
+
+/*function toEnglish(A) {
+    if (A === 10);
+    return "ten";
+}
+
+it ('should change 10 to english "ten"', function () {
+    assert.equal(toEnglish(10), "ten");
+});*/
