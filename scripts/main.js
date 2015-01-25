1efba34d47a2ad918aa688bf8d988f4cfb4511ee
Maddie Fees <madison.fees90@gmail.com>
2015-01-25 15:41:40 -0700

    woohoo I moved the pieces

--- scripts/main.js
+++ scripts/main.js
@@ -10,8 +10,30 @@ var board = [
 
 console.log(board.join('\n') + '\n\n');
 
+
 // Move King's Pawn forward 2
 board[4][4] = board[6][4];
 board[6][4] = ' ';
-console.log(board.join('\n'));
+console.log(board.join('\n') + '\n\n');
+
+
+
+// Trying to move something- WOOHOO!
+board[2][2] = board[1][1];
+board[1][1] = ' ';
+console.log(board.join('\n') + '\n\n');
+
+
+
+// Move lowercase n forward 3 and left 1
+board[4][5] = board[7][6];
+board[7][6] = ' ';
+console.log(board.join('\n') + '\n\n');
+
+
+// Move uppercase R down 2 and left 4
+board[2][3] = board[0][7];
+board[0][7] = ' ';
+console.log(board.join('\n') + '\n\n');
+
     
