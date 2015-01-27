d10a3bbfda63bd9e20bcca37361b3e06680f25e1
Maddie Fees <madison.fees90@gmail.com>
2015-01-26 19:31:28 -0700

    addes require statement and made sure it was linking to the correct file

---
+++ scripts/gymnastics.js
@@ -0,0 +1,17 @@
+var events = require('../apis/github/users/mfees/events.json'); 
+
+console.log(events);
+
+//function answer(){
+//  return {
+//    'total': ...,    // How many total events did you fetch?
+//    'PushEvent': {
+//      'total': ...,  // How many total events of type `PushEvent` are there?
+//      'perDay': ...  // On average, how many`PushEvent` entries per day?
+//    },
+//    'other': {
+//      'total': ...,  // How many _other_ events are in the data?
+//      'perDay': ...  // How many per day, on average?
+//    }
+//  };
+//} // END answer
