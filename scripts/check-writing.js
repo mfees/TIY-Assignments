aabbf569c8c93eaefdf4d17b458b80caf298365f
Maddie Fees <madison.fees90@gmail.com>
2015-01-27 11:56:44 -0700

    commenting out stuff from class, going to rewrite

--- scripts/check-writing.js
+++ scripts/check-writing.js
@@ -1,6 +1,8 @@
 var assert = require('assert');
 
-var toEnglish = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+
+
+/*var toEnglish = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
 
 function counter(a) {
     return toEnglish[a];
@@ -10,4 +12,4 @@ function counter(a) {
 it ('should return a numerical number as an english number', function () {
     assert.equal(counter(0), "zero");
     assert.equal(counter(10), "ten");
-});
+});*/
