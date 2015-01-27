9d183499c75922a1651e06e368783e46fcb5fd6a
Maddie Fees <madison.fees90@gmail.com>
2015-01-27 08:49:38 -0700

    notes from class on apishiz

--- scripts/gymnastics.js
+++ scripts/gymnastics.js
@@ -1,11 +1,37 @@
+var assert = require('assert');
 var events = require('../apis/github/users/mfees/events.json'); 
-
-console.log(events);
 
 function answer(){
   return {
-    'total': ...,    // How many total events did you fetch?
-    'PushEvent': {
+    'total': events.length
+  }
+} // END answer
+
+describe ('the setup', function () {
+    it ('should have events', function () {
+        assert(events);
+    });
+
+    it ('should have an 'answer' function', function () {
+        assert(answer);
+        assert(typeof answer == 'function');
+    });
+});
+
+describe ('the answer' function () {
+    var theAnswer = answer ();
+    
+    it ('should have 30 total events', function () {
+        assert(theAnswer.total === 20);
+    });
+    
+    it ('should have some 'PushEvent' entries', function () {
+        assert(theAnswer.PushEvents.total);
+    });
+});
+
+
+/*    'PushEvent': {
       'total': ...,  // How many total events of type `PushEvent` are there?
       'perDay': ...  // On average, how many`PushEvent` entries per day?
     },
@@ -13,6 +39,4 @@ function answer(){
       'total': ...,  // How many _other_ events are in the data?
       'perDay': ...  // How many per day, on average?
     }
-  };
-} // END answer
- 
+  };*/
