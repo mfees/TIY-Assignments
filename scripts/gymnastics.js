b82fc6f75dd29d3153fc4b94af280b04c4d0a36d
Maddie Fees <madison.fees90@gmail.com>
2015-01-27 13:17:23 -0700

    working together as a group in perfect harmony, soooo much love

--- scripts/gymnastics.js
+++ scripts/gymnastics.js
@@ -1,45 +1,65 @@
 var assert = require('assert');
+
 var events = require('../apis/github/users/mfees/events.json'); 
 
-function answer() {
-    var numberOfPushEvents = 0;
-    
-    var firstEvent = events[0];
-    
-    if( firstEvent.type == "PushEvent") {
-        numberOfPushEvents++;
-    }
-    
-    var secondEvent = event[1];
-    
-    if ( secondEvent = "PushEvent") {
-        numberOfPushEvents++;
-    }
-    
-  return {
-    'total': events.length,
-    'PushEvents': {
-        'total': numberOfPushEvents,
-    }
-  };
-} // END answer
+console.log(Date());
 
-    assert(events);
-    assert(answer);
-    assert(typeof answer == 'function');
-        
-    var theAnswer = answer ();
+    it ('should tell us that events has an imput', function () {
+        assert(events);
+    });
+
+function answer() {
+    return {
+        'total': events.length,
+        'PushEvent': 0,
+    };
+};
+
+console.log(events.length)
+var theAnswer = answer()
+
+    it ('should return that answer exists and test for total events', function () {
+        assert(answer);
+        assert(events.length == 30);
+    });
+
+    it ('should return the length when calling answer function', function () {
+        assert.equal(theAnswer.total, 30);
+    });
+
+    it ('should have "PushEvent"', function () {
+        assert(theAnswer.PushEvent);
+    });
     
-    assert.equal(theAnswer.total, 30);
-    assert(theAnswer.PushEvents);
-    assert(theAnswer.PushEvents.total);
     
-/*    'PushEvent': {
-      'total': ...,  // How many total events of type `PushEvent` are there?
-      'perDay': ...  // On average, how many`PushEvent` entries per day?
-    },
-    'other': {
-      'total': ...,  // How many _other_ events are in the data?
-      'perDay': ...  // How many per day, on average?
-    }
-  };*/
+//function answer() {
+//    'total': 
+//    'PushEvent': {
+//    'total': ...,  // How many total events of type `PushEvent` are there?
+//    'perDay': ...  // On average, how many`PushEvent` entries per day?},
+//    'other': {
+//    'total': ...,  // How many _other_ events are in the data?
+//    'perDay': ...  // How many per day, on average?
+//    }
+//  };    
+    
+    //    var numberOfPushEvents = 0;
+//    
+//    var firstEvent = events[0];
+//    
+//    if( firstEvent.type == "PushEvent") {
+//        numberOfPushEvents++;
+//    }
+//    
+//    var secondEvent = event[1];
+//    
+//    if ( secondEvent = "PushEvent") {
+//        numberOfPushEvents++;
+//    }
+//    
+//  return {
+//    'total': events.length,
+//    'PushEvents': {
+//        'total': numberOfPushEvents,
+//    }
+//  };
