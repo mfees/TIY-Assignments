cb45750f62651f3d634fb0c3866765cecfb76fad
Maddie Fees <madison.fees90@gmail.com>
2015-01-27 19:11:07 -0700

    removing dead code from gymnastics

--- scripts/gymnastics.js
+++ scripts/gymnastics.js
@@ -42,36 +42,3 @@ var theAnswer = answer()
         assert(theAnswer.PushEvent);
         assert(theAnswer.PushEvent.total);
     });
-    
-    
-//function answer() {
-//    'total': 
-//    'PushEvent': {
-//    'total': ...,  // How many total events of type `PushEvent` are there?
-//    'perDay': ...  // On average, how many`PushEvent` entries per day?},
-//    'other': {
-//    'total': ...,  // How many _other_ events are in the data?
-//    'perDay': ...  // How many per day, on average?
-//    }
-//  };    
-    
-    //    var numberOfPushEvents = 0;
-//    
-//    var firstEvent = events[0];
-//    
-//    if( firstEvent.type == "PushEvent") {
-//        numberOfPushEvents++;
-//    }
-//    
-//    var secondEvent = event[1];
-//    
-//    if ( secondEvent = "PushEvent") {
-//        numberOfPushEvents++;
-//    }
-//    
-//  return {
-//    'total': events.length,
-//    'PushEvents': {
-//        'total': numberOfPushEvents,
-//    }
-//  };
