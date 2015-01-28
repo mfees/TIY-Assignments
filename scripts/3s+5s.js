8215524f8b30e2b2efa4fd3152a9be65746481a6
Maddie Fees <madison.fees90@gmail.com>
2015-01-28 15:18:32 -0700

    doing the string calc woohoo

--- scripts/3s+5s.js
+++ scripts/3s+5s.js
@@ -1,49 +1,22 @@
 var assert = require('assert');
 
 function solution (x) {
-//    var countFives = [],
-//        countThrees = [],
-//        fives = 5,
-//        threes = 3,
-//        total = 0;
-//    
-//    while (fives < x) {        
-//        countFives.push(fives);
-//        total = total + fives;
-//        fives = fives + 5;
-//    }
-//    
-//    while (threes < x) {        
-//        countThrees.push(threes);
-//        total = total + threes;
-//        threes = threes + 3;
-//    }
-//    
-//    return total;
-////    var counter = 0;
-////    if (x % 3 == 0 || x % 5 == 0) {
-////        return x;
-////    } 
-    
-        var counter = 0,
-        threes = 0,
-        fives = 0,
-        threesCounter = 0,
-        fivesCounter = 0;
-    
-    
-     while (counter < x) {        
-        if (counter % 3 == 0) {
-            threesCounter = counter/3 + threesCounter;
-        } if (counter % 5 == 0) {
-            fivesCounter = counter/5 + fivesCounter;
+
+    var sum = 0;
+    for(i = 0; i < x; i++) {
+        if (i % 3 === 0 || i % 5 === 0){
+            sum+=i;
         }
-        counter++; 
     }
-    console.log(fivesCounter, threesCounter);
-    return fivesCounter + threesCounter;
+    return sum;
 }
+
 console.log(solution(10));
+console.log(solution(20));
+console.log(solution(30));
+console.log(solution(50));
+console.log(solution(100));
+console.log(solution(1000));
 
 it('should have a `solution` function', function(){
   assert(solution);
@@ -52,16 +25,18 @@ it('should have a `solution` function', function(){
 
 it('should have a `solution` for (10, 20, 30)', function(){
   assert.equal(solution(10), 23);
-  assert.equal(solution(20), undefined);
-  assert.equal(solution(30), undefined);
+  assert.equal(solution(20), 78);
+  assert.equal(solution(30), 195);
 });
 
 it('should have a `solution` for 50', function(){
-  assert.equal(solution(50), undefined);
+  assert.equal(solution(50), 543);
 });
 
 it('should have a solution for 100', function(){
-  assert.equal(solution(100), undefined);
+  assert.equal(solution(100), 2318);
 });
 
-// What about 1000?
+it('should have a solution for 1000', function(){
+  assert.equal(solution(1000), 233168);
+});
