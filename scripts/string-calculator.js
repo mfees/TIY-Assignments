ea30d525fd08c8bcb69b455b266174ebb6116f9e
Maddie Fees <madison.fees90@gmail.com>
2015-01-27 18:24:49 -0700

    makin dat string calulator calculate

--- scripts/string-calculator.js
+++ scripts/string-calculator.js
@@ -14,8 +14,8 @@ assert.equal(plus("one", "one"), 2);
 @return Number sum of A and B
 */
 
-function plus(A) {
-        var numbers = {
+function plus(A, B) {
+    var numbers = {
         "ten": 10,
         "nine": 9,
         "eight": 8,
@@ -27,8 +27,9 @@ function plus(A) {
         "two": 2,
         "one": 1,
         "zero": 0
-    }
-        return numbers[A];
+    };
+
+    return numbers[A] + numbers[B];
 } // END plus
 
 it ('should have a plus function', function () {
@@ -36,7 +37,17 @@ it ('should have a plus function', function () {
     assert.equal(typeof plus, 'function');
 });
 
-it ('should add "zeros"', function () {
+it ('should return a "zero" equal number 0', function () {
     assert.equal(plus("zero", "zero"), 0);
-    assert.equal(plus("zero", "three"), 0);
+    assert.equal(plus("zero", "one"), 1);
+    assert.equal(plus("zero", "two"), 2);
+    assert.equal(plus("zero", "three"), 3);
+    assert.equal(plus("zero", "four"), 4);
+    assert.equal(plus("zero", "five"), 5);
+    assert.equal(plus("zero", "six"), 6);
+    assert.equal(plus("zero", "seven"), 7);
+    assert.equal(plus("zero", "eight"), 8);
+    assert.equal(plus("zero", "nine"), 9);
+    assert.equal(plus("zero", "ten"), 10);
 });
+
