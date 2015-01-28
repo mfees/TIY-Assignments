e083d1e2e49b0ebb07e3a0bdf3cb0e92b891fd18
Maddie Fees <madison.fees90@gmail.com>
2015-01-28 12:38:02 -0700

    adding js provided for the chess board and string calc

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,66 @@
-var board = [
-    ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
-    ['P', 'P', 'P', 'P',  'P', 'P', 'P', 'P'],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',],
-    ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p',],
-    ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'] ];
+function print(board){
+  // Before any rows are printed, print the `<table>`...
+  console.log('<table class="chessboard">');
 
-console.log(board.join('\n') + '\n\n');
+  _.forEach(board, function(row, rank){
 
+      // Before any cells are printed...
 
-// Move King's Pawn forward 2
-board[4][4] = board[6][4];
-board[6][4] = ' ';
-console.log(board.join('\n') + '\n\n');
+      _.forEach(row, function(square, file){
 
+          // In here, we have `rank`, `file`, and `square`...
 
+      }); // END foreach(row)
 
-// Trying to move something- WOOHOO!
-board[2][2] = board[1][1];
-board[1][1] = ' ';
-console.log(board.join('\n') + '\n\n');
+      // After all the cells are printed...
+
+  }); // END foreach(board)
+
+  // After all the rows are printed...
+  console.log('</table> <!-- .chessboard -->');
+} // END print
 
 
 
-// Move lowercase n forward 3 and left 1
-board[4][5] = board[7][6];
-board[7][6] = ' ';
-console.log(board.join('\n') + '\n\n');
 
 
-// Move uppercase R down 2 and left 4
-board[2][3] = board[0][7];
-board[0][7] = ' ';
-console.log(board.join('\n') + '\n\n');
+
+//var board = [
+//    ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
+//    ['P', 'P', 'P', 'P',  'P', 'P', 'P', 'P'],
+//    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',],
+//    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',],
+//    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',],
+//    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',],
+//    ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p',],
+//    ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'] ];
+//
+//console.log(board.join('\n') + '\n\n');
+//
+//
+//// Move King's Pawn forward 2
+//board[4][4] = board[6][4];
+//board[6][4] = ' ';
+//console.log(board.join('\n') + '\n\n');
+//
+//
+//
+//// Trying to move something- WOOHOO!
+//board[2][2] = board[1][1];
+//board[1][1] = ' ';
+//console.log(board.join('\n') + '\n\n');
+//
+//
+//
+//// Move lowercase n forward 3 and left 1
+//board[4][5] = board[7][6];
+//board[7][6] = ' ';
+//console.log(board.join('\n') + '\n\n');
+//
+//
+//// Move uppercase R down 2 and left 4
+//board[2][3] = board[0][7];
+//board[0][7] = ' ';
+//console.log(board.join('\n') + '\n\n');
 
     
