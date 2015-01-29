2a8eb34f877b34c6d6b0b0891a8c0e5431383157
Maddie Fees <madison.fees90@gmail.com>
2015-01-28 21:06:27 -0700

    i just don't even know. literally cannot take in anymore information. I am defeated

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,27 @@
+var assert = require('assert');
+var _ = require('lodash');
+
 function print(board){
+                
   // Before any rows are printed, print the `<table>`...
   console.log('<table class="chessboard">');
 
-  _.forEach(board, function(row, rank){
+  _.forEach(board, function(row, key){
+      
+      console.log(row);
 
+      //console.log('<tr class="row">');
       // Before any cells are printed...
 
-      _.forEach(row, function(square, file){
-
+      _.forEach(row, function(square, k){
+          
+          //console.log('<td class="square"></td>');
+          
           // In here, we have `rank`, `file`, and `square`...
 
       }); // END foreach(row)
-
+      
+      //console.log('</tr>');
       // After all the cells are printed...
 
   }); // END foreach(board)
@@ -21,9 +31,111 @@ function print(board){
 } // END print
 
 
+var board = {
+    'row1': {
+        'square1': 'square1',
+        'square2': 'square2',
+        'square3': 'square3',
+        'square4': 'square4',
+        'square5': 'square5',
+        'square6': 'square6',
+        'square7': 'square7',
+        'square8': 'square8',
+    },
+    'row2': 'two',
+    'row3': 'three',
+    'row4': 'four',
+    'row5': 'five',
+    'row6': 'six',
+    'row7': 'seven',
+    'row8': 'eight'
+};
 
 
 
+print(board);
+
+
+//var func = function(value, key){ 
+//    console.log(value);
+//}
+//
+//
+//
+//_.forEach(theBoard, func);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function print(board){
+//                
+//  // Before any rows are printed, print the `<table>`...
+//  console.log('<table class="chessboard">');
+//
+//  _.forEach(board, function(row, rank){
+//      
+//      console.log('<tr class="row">');
+//      // Before any cells are printed...
+//
+//      _.forEach(row, function(square, file){
+//
+//          // In here, we have `rank`, `file`, and `square`...
+//
+//      }); // END foreach(row)
+//      
+//      console.log('</tr>');
+//      // After all the cells are printed...
+//
+//  }); // END foreach(board)
+//
+//  // After all the rows are printed...
+//  console.log('</table> <!-- .chessboard -->');
+//} // END print
+
+
+
+
+
+
+var row = [
+[ [], [], [], [], [], [], [], [] ],
+[ [], [], [], [], [], [], [], [] ],
+[ [], [], [], [], [], [], [], [] ],
+[ [], [], [], [], [], [], [], [] ],
+[ [], [], [], [], [], [], [], [] ],
+[ [], [], [], [], [], [], [], [] ],
+[ [], [], [], [], [], [], [], [] ],
+[ [], [], [], [], [], [], [], [] ]
+]
+
+
+//for (var i = 0; i < 8; i++) {
+//    board.push(row);
+//}
+//
+//console.log();
 
 //var board = [
 //    ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
