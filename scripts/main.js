dd2e246a7fb9d4a2f33b62d5667e629ea0279b19
Maddie Fees <madison.fees90@gmail.com>
2015-02-02 15:16:17 -0700

    deleting all the pub/ crap

--- scripts/main.js
+++ scripts/main.js
@@ -1,106 +1,45 @@
 var assert = require('assert');
 var _ = require('lodash');
 
-function print(board){
-                
-  // Before any rows are printed, print the `<table>`...
-  console.log('<table class="chessboard">');
-
-  _.forEach(board, function(row, key){
-      
-      console.log(row);
-
-      //console.log('<tr class="row">');
-      // Before any cells are printed...
-
-      _.forEach(row, function(square, k){
-          
-          //console.log('<td class="square"></td>');
-          
-          // In here, we have `rank`, `file`, and `square`...
-
-      }); // END foreach(row)
-      
-      //console.log('</tr>');
-      // After all the cells are printed...
-
-  }); // END foreach(board)
-
-  // After all the rows are printed...
-  console.log('</table> <!-- .chessboard -->');
-} // END print
-
-
-var board = {
-    'row1': {
-        'square1': 'square1',
-        'square2': 'square2',
-        'square3': 'square3',
-        'square4': 'square4',
-        'square5': 'square5',
-        'square6': 'square6',
-        'square7': 'square7',
-        'square8': 'square8',
-    },
-    'row2': 'two',
-    'row3': 'three',
-    'row4': 'four',
-    'row5': 'five',
-    'row6': 'six',
-    'row7': 'seven',
-    'row8': 'eight'
-};
-
-
-
-print(board);
-
-
-//var func = function(value, key){ 
-//    console.log(value);
-//}
-//
-//
-//
-//_.forEach(theBoard, func);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //function print(board){
-//                
-//  // Before any rows are printed, print the `<table>`...
-//  console.log('<table class="chessboard">');
+//    document.write('<table>');
+//    
+//    var cell = "<td></td>";
+//    var row = "<tr></tr>";
+//    
+//    _.times(8, function(){
+//        document.write("<tr>");
+//        _.times(8, function(){
+//            document.write(cell);
+//        });
+//        document.write("</tr>");
+//    });
+//    
+//    document.write('</table>');
+//}
 //
-//  _.forEach(board, function(row, rank){
+//print();
+
+
+document.write(11 + 3);
+           
+
+
+//  // Before any rows are printed, print the `<table>`...
+//  //console.log('<table class="chessboard">');
+//
+//  _.forEach(board, function(row, key){
 //      
+//      console.log(row);
+//
 //      console.log('<tr class="row">');
 //      // Before any cells are printed...
 //
-//      _.forEach(row, function(square, file){
-//
+//      _.forEach(row, function(square, k){
+//          
+//        console.log('<td class="square"></td>');
+//          
 //          // In here, we have `rank`, `file`, and `square`...
 //
 //      }); // END foreach(row)
@@ -113,30 +52,9 @@ print(board);
 //  // After all the rows are printed...
 //  console.log('</table> <!-- .chessboard -->');
 //} // END print
-
-
-
-
-
-
-var row = [
-[ [], [], [], [], [], [], [], [] ],
-[ [], [], [], [], [], [], [], [] ],
-[ [], [], [], [], [], [], [], [] ],
-[ [], [], [], [], [], [], [], [] ],
-[ [], [], [], [], [], [], [], [] ],
-[ [], [], [], [], [], [], [], [] ],
-[ [], [], [], [], [], [], [], [] ],
-[ [], [], [], [], [], [], [], [] ]
-]
-
-
-//for (var i = 0; i < 8; i++) {
-//    board.push(row);
-//}
 //
-//console.log();
-
+//
+//
 //var board = [
 //    ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
 //    ['P', 'P', 'P', 'P',  'P', 'P', 'P', 'P'],
@@ -146,6 +64,10 @@ var row = [
 //    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',],
 //    ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p',],
 //    ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'] ];
+//
+//
+//
+//
 //
 //console.log(board.join('\n') + '\n\n');
 //
@@ -174,5 +96,5 @@ var row = [
 //board[2][3] = board[0][7];
 //board[0][7] = ' ';
 //console.log(board.join('\n') + '\n\n');
-
-    
+//
+//    
