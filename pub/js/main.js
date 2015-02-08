111667e06a7e306ae6449dc1d28e15dd9611b372
Maddie Fees <madison.fees90@gmail.com>
2015-02-07 19:31:30 -0700

    adding more user information, unsure of how to get emails to mail to user's email, also that time...yikes

--- pub/js/main.js
+++ pub/js/main.js
@@ -5,6 +5,11 @@ $(function() {
         user = data;
 
         $('.name').text(user.name);
+        $('.user-name').text(user.login);
+        $('.company').text(user.company);
+        $('.location').text(user.location);
+        $('.email').text(user.email);
+        $('.joined').text(user.created_at);
         
         console.log(data);
     });
