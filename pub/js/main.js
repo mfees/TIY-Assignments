39ddc2b8b47d664e5715e7a8604dac3704b7be16
Maddie Fees <madison.fees90@gmail.com>
2015-02-07 18:33:57 -0700

    adding name with json file

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,5 +1,11 @@
 $(function() {
+    var user;
+
     $.get( "https://api.github.com/users/mfees", function( data ) {
+        user = data;
+
+        $('.name').text(user.name);
+        
         console.log(data);
     });
 });
