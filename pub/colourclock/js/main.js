46135d2965c6a58c6ca0a65aae53f93e6727e099
Maddie Fees <madison.fees90@gmail.com>
2015-02-09 20:56:16 -0700

    added hexidecimal numbers, that was annoying, made a cap F on the math.floor and it broke everything, note to self, don't do that again

--- pub/colourclock/js/main.js
+++ pub/colourclock/js/main.js
@@ -1,4 +1,4 @@
-var d, h, m, s, color;
+var d, h, m, s, hexHours, hexMinutes, hexSecs, color;
 
 function countUp() {
     d = new Date();
@@ -7,21 +7,31 @@ function countUp() {
     s = d.getSeconds();
     
     
+    hexHours = (Math.floor(h * 255/23)).toString(16);
+    hexMinutes = (Math.floor(m * 255/59)).toString(16);
+    hexSecs = (Math.floor(s * 255/59)).toString(16);
+    
+    
     if(h <= 9) h = '0' + h;
     if(m <= 9) m = '0' + m;
     if(s <= 9) s = '0' + s;
     
-    color = "#" + h + m + s;
-        
+    
+    if(hexHours.length < 2) hexHours = '0' + hexHours;
+    if(hexMinutes.length < 2) hexMinutes = '0' + hexMinutes;
+    if(hexSecs.length < 2) hexSecs = '0' + hexSecs;
+    
+    
+    color = "#" + hexHours + hexMinutes + hexSecs;
+
     
     document.body.style.background = color;
     
-//    document.getElementById("date").innerHTML =
-//    color;
     document.getElementById("date").innerHTML = h + ":" + m + ":" + s;
     
     setInterval(countUp, 1000);
-}
+    
+    console.log(countUp);
+};
 
-countUp ();
-
+countUp();
