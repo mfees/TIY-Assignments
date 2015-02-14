1d1be90869cd815953afffece2d729f302f2cef6
Maddie Fees <madison.fees90@gmail.com>
2015-02-14 13:38:55 -0700

    String Calculator: adding an object to plus function for strings one through nine

--- pub/string-calculator/js/main.js
+++ pub/string-calculator/js/main.js
@@ -2,7 +2,19 @@ var assert = require('assert');
 
 console.log(Date());
 
-function plus() {
+function plus(a, b) {
+    var number= {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9
+    };
+    return number[a] + [b];
 };
 
 
@@ -10,3 +22,5 @@ it('should have a plus function', function () {
     assert(plus);
     assert.equal(typeof plus, "function");
 });
+
+
