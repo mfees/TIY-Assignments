580359d9b8140033097f831741bee1e24d2c037f
Maddie Fees <madison.fees90@gmail.com>
2015-02-14 13:20:24 -0700

    String Calculator: making function and first assert

--- pub/string-calculator/js/main.js
+++ pub/string-calculator/js/main.js
@@ -1,3 +1,12 @@
 var assert = require('assert');
 
 console.log(Date());
+
+function plus() {
+};
+
+
+it('should have a plus function', function () {
+    assert(plus);
+    assert.equal(typeof plus, "function");
+});
