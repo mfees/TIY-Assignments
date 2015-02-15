15878cb5c0daf19e71add26ba17ff8b89d797482
Maddie Fees <madison.fees90@gmail.com>
2015-02-15 10:48:29 -0700

    getting the check writing to work

--- pub/check-writing/js/main.js
+++ pub/check-writing/js/main.js
@@ -28,11 +28,28 @@ function toEnglish(a) {
         16: "sixteen",
         17: "seventeen",
         18: "eighteen",
-        19: "ninteen"
+        19: "ninteen",
+        20: "twenty",
+        30: "thirty",
+        40: "forty",
+        50: "fifty",
+        60: "sixty",
+        70: "seventy",
+        80: "eighty",
+        90: "ninety"
     };
-        return number[a];
-}; //END toEnglish
     
+//    console.log("In toEnglish: " + a);
+    if (a < 20)
+        return number[a];
+    
+    if (Math.floor(a / 10) * 10 == a)
+        return number[a];
+    
+    return number[Math.floor(a/10)*10] + " " + number[a % 10];
+}; //END toEnglish
+  
+
 it('should make 4 equal "four"', function () {
     assert.equal(toEnglish(4), "four");
 });
@@ -51,18 +68,56 @@ console.log(toEnglish(12));
  * @param Number n representing a dollar value
  * @returns String of English words for `n`
  */
-function toCheck(n) {  
-    if(n < 10);
-        return number[n] + " &" + "/100s dollars";
+function toCheck(n) { 
+    var x = "";
+    
+//    console.log(n % 1000000 / 1000);
+    
+    if(n % 1000000 / 1000 > 1)
+        x = x + toEnglish(Math.floor(n % 1000000/ 1000)) + " thousand ";
+    
+//    console.log(Math.floor(n % 1000 / 100));
+    
+    if(n % 1000 / 100 > 1)
+        x = x + toEnglish(Math.floor(n % 1000/ 100)) + " hundred ";
+    
+//    console.log(Math.floor(n % 100 / 1));
+    
+    if (n % 100 / 1 > 1)
+        x = x + toEnglish(Math.floor(n % 100 / 1));
+
+    return x + " & " + Math.round(n % 1 * 100) + "/100s dollars";
 }; // END toCheck
+
 
 it('should equal "one & 23/100s dollars"', function () {
     assert.equal(toCheck(1.23), "one & 23/100s dollars");
 });
-console.log(toCheck(1));
+
+console.log(toCheck(1.23));
 
 
-//assert.equal(toCheck(12.34), "twelve & 34/100s dollars");
-//assert.equal(toCheck(123.45), "one hundred twenty three & 34/100s dollars");
-//assert.equal(toCheck(1234.56), "one thousand two hundred thirty four & 56/100s dollars");
-//assert.equal(toCheck(12345.67), "twelve thousand three hundred forty five & 67/100s dollars");
+it('should equal "twelve & 34/100s dollars"', function () {
+    assert.equal(toCheck(12.34), "twelve & 34/100s dollars");
+});
+
+console.log(toCheck(12.34));
+
+
+it('should equal "one hundred twenty three & 45/100s dollars"', function () {
+    assert.equal(toCheck(123.45), "one hundred twenty three & 45/100s dollars");
+});
+
+console.log(toCheck(123.45));
+
+it('should equal "one thousand two hundred thirty four & 56/100s dollars"', function () {
+    assert.equal(toCheck(1234.56), "one thousand two hundred thirty four & 56/100s dollars");
+});
+
+console.log(toCheck(1234.56));
+
+it('should equal "twelve thousand three hundred forty five & 67/100s dollars"', function () {
+    assert.equal(toCheck(12345.67), "twelve thousand three hundred forty five & 67/100s dollars");
+});
+
+console.log(toCheck(12345.67));
