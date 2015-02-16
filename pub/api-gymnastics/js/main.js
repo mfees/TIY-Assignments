6e23491293f77b7a7df63fab6378e5ccb05d9d63
Maddie Fees <madison.fees90@gmail.com>
2015-02-15 18:39:55 -0700

    trying to get the other events total

--- pub/api-gymnastics/js/main.js
+++ pub/api-gymnastics/js/main.js
@@ -7,11 +7,12 @@ console.log(Date());
 
 var pEvents = _.filter(events, {
     'type': 'PushEvent' 
-});
-    
+}); 
+
+var otherEvents = events.length - pEvents.length;
     
 function answer(){
-  return {
+    return {
     'total': events.length,    // How many total events did you fetch?
     'PushEvent': {
       'total': pEvents.length  // How many total events of type `PushEvent` are there?
@@ -21,12 +22,16 @@ function answer(){
 //      'total': ...,  // How many _other_ events are in the data?
 //      'perDay': ...  // How many per day, on average?
 //    }
-  };
+    };
 } // END answer
+
 
 console.log(answer);
 console.log(events.length);
 console.log(pEvents.length);
+console.log(events.length - pEvents.length);
+console.log(otherEvents.length);
+
 
 it('should show the length of the array "events"', function () {
     assert.equal(events.length, 30);
@@ -34,6 +39,10 @@ it('should show the length of the array "events"', function () {
 
 it('should show the length of the number of "pushevents"', function () {
     assert.equal(pEvents.length, 10);
+});
+
+it('should return the number of events that are not push events', function () {
+    assert.equal(otherEvents.length, 20);
 });
     
 
