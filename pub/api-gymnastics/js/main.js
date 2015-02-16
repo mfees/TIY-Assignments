96743ed01510a9c86b562261f9318774813fed83
Maddie Fees <madison.fees90@gmail.com>
2015-02-15 18:09:17 -0700

    fetching number of push events

--- pub/api-gymnastics/js/main.js
+++ pub/api-gymnastics/js/main.js
@@ -4,13 +4,19 @@ var assert = require('assert'),
 
 console.log(Date());
 
+
+var pEvents = _.filter(events, {
+    'type': 'PushEvent' 
+});
+    
+    
 function answer(){
   return {
-    'total': events.length    // How many total events did you fetch?
-//    'PushEvent': {
-//      'total': ...,  // How many total events of type `PushEvent` are there?
+    'total': events.length,    // How many total events did you fetch?
+    'PushEvent': {
+      'total': pEvents.length  // How many total events of type `PushEvent` are there?
 //      'perDay': ...  // On average, how many`PushEvent` entries per day?
-//    },
+    },
 //    'other': {
 //      'total': ...,  // How many _other_ events are in the data?
 //      'perDay': ...  // How many per day, on average?
@@ -20,9 +26,15 @@ function answer(){
 
 console.log(answer);
 console.log(events.length);
+console.log(pEvents.length);
 
 it('should show the length of the array "events"', function () {
     assert.equal(events.length, 30);
 });
+
+it('should show the length of the number of "pushevents"', function () {
+    assert.equal(pEvents.length, 10);
+});
+    
 
 
