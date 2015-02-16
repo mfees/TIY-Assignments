0997e800f12a099b8b318e2f1aaea29a3fd67175
Maddie Fees <madison.fees90@gmail.com>
2015-02-15 17:28:41 -0700

    adding npm modules and trying to link up lodash

--- pub/api-gymnastics/js/main.js
+++ pub/api-gymnastics/js/main.js
@@ -1,19 +1,23 @@
 var assert = require('assert'),
     _ = require('lodash'),
-    events = require('../apis/github/users/mfees/events.json'); 
+    events = require('../../apis/github/users/mfees/events.json'); 
 
 console.log(Date());
 
 function answer(){
-  return {
-    'total': ...,    // How many total events did you fetch?
-    'PushEvent': {
-      'total': ...,  // How many total events of type `PushEvent` are there?
-      'perDay': ...  // On average, how many`PushEvent` entries per day?
-    },
-    'other': {
-      'total': ...,  // How many _other_ events are in the data?
-      'perDay': ...  // How many per day, on average?
-    }
-  };
+//  return {
+//    'total': ...,    // How many total events did you fetch?
+//    'PushEvent': {
+//      'total': ...,  // How many total events of type `PushEvent` are there?
+//      'perDay': ...  // On average, how many`PushEvent` entries per day?
+//    },
+//    'other': {
+//      'total': ...,  // How many _other_ events are in the data?
+//      'perDay': ...  // How many per day, on average?
+//    }
+//  };
 } // END answer
+
+console.log(answer);
+
+
