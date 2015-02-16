841469eb6a2b67a9b6800811d6181773dfc61b7c
Maddie Fees <madison.fees90@gmail.com>
2015-02-15 17:59:54 -0700

    finding events.length

--- pub/api-gymnastics/js/main.js
+++ pub/api-gymnastics/js/main.js
@@ -5,8 +5,8 @@ var assert = require('assert'),
 console.log(Date());
 
 function answer(){
-//  return {
-//    'total': ...,    // How many total events did you fetch?
+  return {
+    'total': events.length    // How many total events did you fetch?
 //    'PushEvent': {
 //      'total': ...,  // How many total events of type `PushEvent` are there?
 //      'perDay': ...  // On average, how many`PushEvent` entries per day?
@@ -15,10 +15,14 @@ function answer(){
 //      'total': ...,  // How many _other_ events are in the data?
 //      'perDay': ...  // How many per day, on average?
 //    }
-//  };
+  };
 } // END answer
 
 console.log(answer);
 console.log(events.length);
+
+it('should show the length of the array "events"', function () {
+    assert.equal(events.length, 30);
+});
 
 
