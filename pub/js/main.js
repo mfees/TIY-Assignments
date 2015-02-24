f7d695c32762f43b239e3563baf6001e6368c7db
Maddie Fees <madison.fees90@gmail.com>
2015-02-23 17:25:02 -0700

    putting answers from level one angular code school in pub folder

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,1 +1,46 @@
+Answer one:
+var app = angular.module("gemStore", [ ]);
 
+
+Answer two:
+(function(){
+    var gem = { name: 'Azurite', price: 2.95 };
+    var app = angular.module('gemStore', []);
+    app.controller('StoreController', function() {
+        this.product = gem;
+    });
+})();
+
+
+Answer three:
+They added the canPurchase and soldOut data to var
+(function() {
+    var app = angular.module('gemStore', []);
+
+    app.controller('StoreController', function(){
+        this.product = gem;
+    });
+
+    var gem = {
+        name: 'Azurite',
+        price: 110.50,
+        canPurchase: false,
+        soldOut: true
+    };
+})();
+
+
+Answer four:
+(function() {
+    var app = angular.module('gemStore', []);
+
+    app.controller('StoreController', function(){
+        this.products = gems;
+    });
+
+    var gems = [
+        { name: 'Azurite', price: 2.95 },
+        { name: 'Bloodstone', price: 5.95 },
+        { name: 'Zircon', price: 3.95 }
+    ];
+})();
